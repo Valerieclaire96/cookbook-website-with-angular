de8a69df31c20ba9d909eadf940b58e1450b3b8d
valerieclaire96 <valerieclaire96@gmail.com>
2023-05-31 13:22:43 -0400

    header static complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shoppingList/shopping-list.component';
+import { ShoppingListEditComponent } from './shoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -18,8 +18,8 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
     ShoppingListComponent,
     ShoppingListEditComponent,
     RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
